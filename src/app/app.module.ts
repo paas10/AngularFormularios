6bfa1f1edc16af22c996685a1245a4dfae80a2af
Pablo <pablo.andre_alvarado@hotmail.com>
2022-01-13 18:10:32 -0600

    [Created]: Project Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+import { FormularioBasicoComponent } from './formulario-basico/formulario-basico.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioBasicoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
